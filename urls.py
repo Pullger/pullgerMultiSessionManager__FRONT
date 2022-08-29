3bfd246dc9f534a4baea43eb10d7e0f1506d1465
NeoUKR <neokkv@gmail.com>
2022-08-29 21:35:20 +0300

    - Initialization

--- urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from pullgerMultisessionManager_FRONT import views
-
-urlpatterns = [
-    path('about/', views.about),
-]
---
+++ version.py
@@ -0,0 +1,6 @@
+VERSION = (0, 0, 0, 0)
+VERSION_INFO = '.'.join(str(nv) for nv in VERSION)
+__version__ = VERSION_INFO
+
+# 0.0.0.0
+#   - Initialization
